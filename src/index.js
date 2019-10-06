33e46b5d83f951df80320776d1d4d15a8c54fcbd
shibayu36 <shibayu36@gmail.com>
2019-10-06 10:57:42 +0900

    ボタンを押すとXが表示されるように

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
-        {/* TODO */}
+      <button className="square" onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
       </button>
     );
   }
